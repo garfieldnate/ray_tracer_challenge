849ade0077e3572145958d4104acf6ab58eeaa23
Nathan Glenn <garfieldnate@gmail.com>
2020-02-06 21:51:46 +0100

    Canvas to PPM work in progress

--- src/canvas.rs
+++ src/canvas.rs
@@ -25,7 +25,7 @@ impl Canvas {
 
 	fn to_ppm(&self) -> String {
 		let mut ppm = String::new();
-		// header is "P3", width/height, color max values
+		// header is "P3", width/height, and color max value
 		ppm.push_str("P3\n");
 		ppm.push_str(&(self.width as u32).to_string());
 		ppm.push(' ');
@@ -33,6 +33,24 @@ impl Canvas {
 		ppm.push('\n');
 		ppm.push_str("255\n");
 
+		// write pixel data
+		for row in 0..self.height {
+			for column in 0..self.width {
+				let color = self.pixel_at(column, row);
+				// scale and clamp color valuesat 255
+				let r = (color.r * 255.0).min(255.0).max(0.0) as u8;
+				let g = (color.g * 255.0).min(255.0).max(0.0) as u8;
+				let b = (color.b * 255.0).min(255.0).max(0.0) as u8;
+
+				ppm.push_str(&r.to_string());
+				ppm.push(' ');
+				ppm.push_str(&g.to_string());
+				ppm.push(' ');
+				ppm.push_str(&b.to_string());
+				ppm.push(' ');
+			}
+			ppm.push('\n');
+		}
 		ppm
 	}
 }
@@ -63,5 +81,25 @@ mod tests {
 		assert_eq!(lines.next().unwrap(), "P3");
 		assert_eq!(lines.next().unwrap(), "20 5");
 		assert_eq!(lines.next().unwrap(), "255");
+	}
+
+	#[test]
+	fn test_ppm_pixel_data() {
+		let mut c = build_canvas(5, 3);
+		c.write_pixel(0, 0, build_color(1.5, 0.0, 0.0));
+		c.write_pixel(2, 1, build_color(0.0, 0.5, 0.0));
+		c.write_pixel(4, 2, build_color(-0.5, 0.0, 1.0));
+
+		let ppm = c.to_ppm();
+		let mut lines = ppm.lines();
+		// ignore header
+		lines.next();
+		lines.next();
+		lines.next();
+		NEXT: get rid of trailing spaces, then break lines at 70 characters
+		assert_eq!(lines.next().unwrap(), "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+		// book says 128, but I'll trust Rust's rounding for now
+		assert_eq!(lines.next().unwrap(), "0 0 0 0 0 0 0 127 0 0 0 0 0 0 0");
+		assert_eq!(lines.next().unwrap(), "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255");
 	}
 }
--- src/color.rs
+++ src/color.rs
@@ -7,122 +7,122 @@ use std::ops::Sub;
 // TODO: allow changing datatypes to f64?
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Color {
-	r: f32,
-	g: f32,
-	b: f32,
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
 }
 
 pub fn build_color(r: f32, g: f32, b: f32) -> Color {
-	Color { r: r, g: g, b: b }
+    Color { r: r, g: g, b: b }
 }
 
 impl Add for Color {
-	type Output = Color;
-	fn add(self, other: Color) -> Color {
-		Color {
-			r: self.r + other.r,
-			g: self.g + other.g,
-			b: self.b + other.b,
-		}
-	}
+    type Output = Color;
+    fn add(self, other: Color) -> Color {
+        Color {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+        }
+    }
 }
 
 impl Sub for Color {
-	type Output = Color;
-	fn sub(self, other: Color) -> Color {
-		Color {
-			r: self.r - other.r,
-			g: self.g - other.g,
-			b: self.b - other.b,
-		}
-	}
+    type Output = Color;
+    fn sub(self, other: Color) -> Color {
+        Color {
+            r: self.r - other.r,
+            g: self.g - other.g,
+            b: self.b - other.b,
+        }
+    }
 }
 
 impl Mul<f32> for Color {
-	type Output = Color;
-	fn mul(self, scalar: f32) -> Color {
-		Color {
-			r: self.r * scalar,
-			g: self.g * scalar,
-			b: self.b * scalar,
-		}
-	}
+    type Output = Color;
+    fn mul(self, scalar: f32) -> Color {
+        Color {
+            r: self.r * scalar,
+            g: self.g * scalar,
+            b: self.b * scalar,
+        }
+    }
 }
 
 impl Mul<Color> for f32 {
-	type Output = Color;
-	fn mul(self, color: Color) -> Color {
-		color * self
-	}
+    type Output = Color;
+    fn mul(self, color: Color) -> Color {
+        color * self
+    }
 }
 
 // Hadamard product for mixing two colors
 impl Mul<Color> for Color {
-	type Output = Color;
-	fn mul(self, other: Color) -> Color {
-		Color {
-			r: self.r * other.r,
-			g: self.g * other.g,
-			b: self.b * other.b,
-		}
-	}
+    type Output = Color;
+    fn mul(self, other: Color) -> Color {
+        Color {
+            r: self.r * other.r,
+            g: self.g * other.g,
+            b: self.b * other.b,
+        }
+    }
 }
 
 impl Div<f32> for Color {
-	type Output = Color;
-	fn div(self, scalar: f32) -> Color {
-		Color {
-			r: self.r / scalar,
-			g: self.g / scalar,
-			b: self.b / scalar,
-		}
-	}
+    type Output = Color;
+    fn div(self, scalar: f32) -> Color {
+        Color {
+            r: self.r / scalar,
+            g: self.g / scalar,
+            b: self.b / scalar,
+        }
+    }
 }
 
 // required for approximate comparisons due to use of floating point numbers
 impl AbsDiffEq for Color {
-	type Epsilon = f32;
+    type Epsilon = f32;
 
-	fn default_epsilon() -> Self::Epsilon {
-		f32::default_epsilon()
-	}
+    fn default_epsilon() -> Self::Epsilon {
+        f32::default_epsilon()
+    }
 
-	fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
-		f32::abs_diff_eq(&self.r, &other.r, epsilon)
-			&& f32::abs_diff_eq(&self.g, &other.g, epsilon)
-			&& f32::abs_diff_eq(&self.b, &other.b, epsilon)
-	}
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        f32::abs_diff_eq(&self.r, &other.r, epsilon)
+            && f32::abs_diff_eq(&self.g, &other.g, epsilon)
+            && f32::abs_diff_eq(&self.b, &other.b, epsilon)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	// TODO: lots of fails because of lack of approximate equality
+    use super::*;
+    // TODO: lots of fails because of lack of approximate equality
 
-	#[test]
-	fn test_adding_colors() {
-		let c1 = build_color(0.9, 0.6, 0.75);
-		let c2 = build_color(0.7, 0.1, 0.25);
-		abs_diff_eq!(c1 + c2, build_color(1.6, 0.7, 1.0));
-	}
+    #[test]
+    fn test_adding_colors() {
+        let c1 = build_color(0.9, 0.6, 0.75);
+        let c2 = build_color(0.7, 0.1, 0.25);
+        abs_diff_eq!(c1 + c2, build_color(1.6, 0.7, 1.0));
+    }
 
-	#[test]
-	fn test_subtracting_colors() {
-		let c1 = build_color(0.9, 0.6, 0.75);
-		let c2 = build_color(0.7, 0.1, 0.25);
-		abs_diff_eq!(c1 - c2, build_color(0.2, 0.5, 0.5));
-	}
+    #[test]
+    fn test_subtracting_colors() {
+        let c1 = build_color(0.9, 0.6, 0.75);
+        let c2 = build_color(0.7, 0.1, 0.25);
+        abs_diff_eq!(c1 - c2, build_color(0.2, 0.5, 0.5));
+    }
 
-	#[test]
-	fn test_multiply_color_by_scalar() {
-		let c = build_color(0.2, 0.3, 0.4);
-		abs_diff_eq!(c * 2.0, 2.0 * c);
-		abs_diff_eq!(c * 2.0, build_color(0.4, 0.6, 0.8));
-	}
-	#[test]
-	fn test_mix_colors_by_multiplication() {
-		let c1 = build_color(1.0, 0.2, 0.4);
-		let c2 = build_color(0.9, 1.0, 0.1);
-		abs_diff_eq!(c1 * c2, build_color(0.9, 0.2, 0.04));
-	}
+    #[test]
+    fn test_multiply_color_by_scalar() {
+        let c = build_color(0.2, 0.3, 0.4);
+        abs_diff_eq!(c * 2.0, 2.0 * c);
+        abs_diff_eq!(c * 2.0, build_color(0.4, 0.6, 0.8));
+    }
+    #[test]
+    fn test_mix_colors_by_multiplication() {
+        let c1 = build_color(1.0, 0.2, 0.4);
+        let c2 = build_color(0.9, 1.0, 0.1);
+        abs_diff_eq!(c1 * c2, build_color(0.9, 0.2, 0.04));
+    }
 }
