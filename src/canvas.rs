7a9d5f9926542cea90b31b365061efd890437194
Nathan Glenn <garfieldnate@gmail.com>
2020-02-06 20:38:48 +0100

    PPM header

--- src/canvas.rs
+++ src/canvas.rs
@@ -6,7 +6,7 @@ struct Canvas {
 	data: Vec<Vec<Color>>,
 }
 
-fn build_canvas(height: usize, width: usize) -> Canvas {
+fn build_canvas(width: usize, height: usize) -> Canvas {
 	Canvas {
 		width: width,
 		height: height,
@@ -22,6 +22,19 @@ impl Canvas {
 	fn pixel_at(&self, x: usize, y: usize) -> Color {
 		self.data[x][y]
 	}
+
+	fn to_ppm(&self) -> String {
+		let mut ppm = String::new();
+		// header is "P3", width/height, color max values
+		ppm.push_str("P3\n");
+		ppm.push_str(&(self.width as u32).to_string());
+		ppm.push(' ');
+		ppm.push_str(&(self.height as u32).to_string());
+		ppm.push('\n');
+		ppm.push_str("255\n");
+
+		ppm
+	}
 }
 
 #[cfg(test)]
@@ -29,9 +42,9 @@ mod tests {
 	use super::*;
 	#[test]
 	fn test_height_and_width() {
-		let c = build_canvas(10, 10);
+		let c = build_canvas(15, 10);
+		assert_eq!(c.width, 15);
 		assert_eq!(c.height, 10);
-		assert_eq!(c.width, 10);
 	}
 
 	#[test]
@@ -40,5 +53,15 @@ mod tests {
 		let color = build_color(0.1, 0.2, 0.3);
 		canvas.write_pixel(3, 4, color);
 		assert_eq!(canvas.pixel_at(3, 4), color);
+	}
+
+	#[test]
+	fn test_ppm_header() {
+		let c = build_canvas(20, 5);
+		let ppm = c.to_ppm();
+		let mut lines = ppm.lines();
+		assert_eq!(lines.next().unwrap(), "P3");
+		assert_eq!(lines.next().unwrap(), "20 5");
+		assert_eq!(lines.next().unwrap(), "255");
 	}
 }
