bc6d33aeb987b16ba75ff2aecb89c5c179b2d086
Nathan Glenn <nathan-gary.glenn@trivago.com>
2020-02-13 18:01:32 +0100

    Much improved documentation

--- src/material.rs
+++ src/material.rs
@@ -1,35 +1,42 @@
 use crate::color::build_color;
 use crate::color::Color;
 
+// Represents the reflective properties of a surface
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Material {
-    pub color: Color,
-    pub ambient: f32,
-    pub diffuse: f32,
-    pub specular: f32,
-    pub shininess: f32,
+	pub color: Color,
+	// light reflected from other objects in the environment [0,1]
+	pub ambient: f32,
+	// light reflected from a matte surface; depends on angle between
+	// light source and surface normal [0,1]
+	pub diffuse: f32,
+	// the reflection of the light source itself (gives specular highlight);
+	// depends on the angle between the reflection vector and the eye vector [0,1]
+	pub specular: f32,
+	// higher values give smaller and tighter specular highlights [10,200] (no real upper bound)
+	pub shininess: f32,
 }
 
 pub fn build_material() -> Material {
-    Material {
-        color: build_color(1.0, 1.0, 1.0),
-        ambient: 0.1,
-        diffuse: 0.9,
-        specular: 0.9,
-        shininess: 200.0,
-    }
+	Material {
+		color: build_color(1.0, 1.0, 1.0),
+		ambient: 0.1,
+		diffuse: 0.9,
+		specular: 0.9,
+		shininess: 200.0,
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    #[test]
-    fn default_material_attributes() {
-        let m = build_material();
-        assert_eq!(m.color, build_color(1.0, 1.0, 1.0));
-        assert_eq!(m.ambient, 0.1);
-        assert_eq!(m.diffuse, 0.9);
-        assert_eq!(m.specular, 0.9);
-        assert_eq!(m.shininess, 200.0);
-    }
+	use super::*;
+	#[test]
+	fn default_material_attributes() {
+		let m = build_material();
+		assert_eq!(m.color, build_color(1.0, 1.0, 1.0));
+		assert_eq!(m.ambient, 0.1);
+		assert_eq!(m.diffuse, 0.9);
+		assert_eq!(m.specular, 0.9);
+		assert_eq!(m.shininess, 200.0);
+	}
 }
