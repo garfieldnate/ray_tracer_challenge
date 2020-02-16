7e53bd8cbb10cd9827e02cb699c66c5010729240
Nathan Glenn <garfieldnate@gmail.com>
2020-02-16 13:52:28 +0100

    replace build_canvase with Canvas::new

--- src/canvas.rs
+++ src/canvas.rs
@@ -6,21 +6,20 @@ pub struct Canvas {
     data: Vec<Vec<Color>>,
 }
 
-// Create a canvas initialized to all black
-pub fn build_canvas(width: usize, height: usize) -> Canvas {
-    Canvas {
-        width: width,
-        height: height,
-        data: vec![vec![color!(0, 0, 0); width]; height],
-    }
-}
-
 const MAX_COLOR_VAL: i16 = 255;
 const MAX_PPM_LINE_LENGTH: usize = 70;
 // length of "255" is 3
 // TODO: this should be evaluated programmatically, but "no matching in consts allowed" error prevented this
 const MAX_COLOR_VAL_STR_LEN: usize = 3;
 impl Canvas {
+    // Create a canvas initialized to all black
+    pub fn new(width: usize, height: usize) -> Canvas {
+        Canvas {
+            width: width,
+            height: height,
+            data: vec![vec![color!(0, 0, 0); width]; height],
+        }
+    }
     pub fn write_pixel(&mut self, x: usize, y: usize, color: Color) -> () {
         if x <= self.width && y <= self.height {
             self.data[y][x] = color;
@@ -99,14 +98,14 @@ mod tests {
     use super::*;
     #[test]
     fn test_height_and_width() {
-        let c = build_canvas(15, 10);
+        let c = Canvas::new(15, 10);
         assert_eq!(c.width, 15);
         assert_eq!(c.height, 10);
     }
 
     #[test]
     fn test_write_and_read_pixels() {
-        let mut canvas = build_canvas(10, 5);
+        let mut canvas = Canvas::new(10, 5);
         let color = color!(0.1, 0.2, 0.3);
         canvas.write_pixel(7, 4, color);
         assert_eq!(canvas.pixel_at(7, 4), color);
@@ -114,7 +113,7 @@ mod tests {
 
     #[test]
     fn test_ppm_header() {
-        let c = build_canvas(20, 5);
+        let c = Canvas::new(20, 5);
         let ppm = c.to_ppm();
         let mut lines = ppm.lines();
         assert_eq!(lines.next().unwrap(), "P3");
@@ -124,7 +123,7 @@ mod tests {
 
     #[test]
     fn test_ppm_pixel_data() {
-        let mut c = build_canvas(5, 3);
+        let mut c = Canvas::new(5, 3);
         c.write_pixel(0, 0, color!(1.5, 0, 0));
         c.write_pixel(2, 1, color!(0, 0.5, 0));
         c.write_pixel(4, 2, color!(-0.5, 0, 1));
@@ -143,7 +142,7 @@ mod tests {
 
     #[test]
     fn test_splitting_long_ppm_lines() {
-        let mut canvas = build_canvas(10, 2);
+        let mut canvas = Canvas::new(10, 2);
         let color = color!(1, 0.8, 0.6);
         // TODO: maybe turn this into a function on canvas?
         for row in 0..canvas.height {
