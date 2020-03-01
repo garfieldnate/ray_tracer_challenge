6347cfa4906007473b8b1d39d6c6437bacf21bc2
Nathan Glenn <garfieldnate@gmail.com>
2020-03-01 10:46:33 +0100

    No need to use actual files for testing
    since strings also implement the Read trait.

--- src/obj_parser.rs
+++ src/obj_parser.rs
@@ -12,7 +12,6 @@ where
     let mut num_ignored_lines = 0;
     for line in buf_reader.lines() {
         num_ignored_lines += 1;
-        println!("{}", line.unwrap());
     }
     ObjParseResults { num_ignored_lines }
 }
@@ -20,29 +19,17 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::{self, Write};
-    use tempfile::NamedTempFile;
 
     #[test]
-    fn ignoring_unrecognized_files() -> io::Result<()> {
-        let text = "There was a young lady named Bright
+    fn ignoring_unrecognized_files() {
+        let mut text = "There was a young lady named Bright
             who traveled much faster than light.
             She set out one day
             in a relative way,
-            and came back the previous night.";
-
-        // Create a file inside of `std::env::temp_dir()`.
-        let mut file1 = NamedTempFile::new()?;
-        // Re-open it.
-        let mut file2 = file1.reopen()?;
-
-        // Write some test data to the first handle.
-        file1.write_all(text.as_bytes())?;
-
-        let results = parse_obj(&mut file2);
+            and came back the previous night."
+            .as_bytes();
+        let results = parse_obj(&mut text);
 
         assert_eq!(results.num_ignored_lines, 5);
-
-        Ok(())
     }
 }
