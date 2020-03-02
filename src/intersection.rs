17f683cbedbee9719d220c19ec0f532b1301c5d2
Nathan Glenn <garfieldnate@gmail.com>
2020-03-02 19:54:17 +0100

    Add u and v fields to Intersection

--- src/intersection.rs
+++ src/intersection.rs
@@ -5,11 +5,26 @@ use std::cmp::Ordering::Equal;
 pub struct Intersection<'a> {
     pub distance: f32,
     pub object: &'a dyn Shape,
+    pub u: f32,
+    pub v: f32,
 }
 
 impl Intersection<'_> {
     pub fn new(distance: f32, object: &dyn Shape) -> Intersection {
-        Intersection { distance, object }
+        Intersection {
+            distance,
+            object,
+            u: 0.,
+            v: 0.,
+        }
+    }
+    pub fn new_with_uv(distance: f32, object: &dyn Shape, u: f32, v: f32) -> Intersection {
+        Intersection {
+            distance,
+            object,
+            u,
+            v,
+        }
     }
     // returns the a reference to the intersection with the lowest non-negative distance (or None if all are negative)
     pub fn hit<'a>(intersections: &'a [Intersection<'a>]) -> Option<&'a Intersection<'a>> {
@@ -38,14 +53,8 @@ mod tests {
     #[test]
     fn hit_all_intersections_have_positive_distance() {
         let s = Sphere::new();
-        let i1 = Intersection {
-            distance: 1.0,
-            object: &s,
-        };
-        let i2 = Intersection {
-            distance: 2.0,
-            object: &s,
-        };
+        let i1 = Intersection::new(1.0, &s);
+        let i2 = Intersection::new(2.0, &s);
 
         let intersections = vec![i1, i2];
         let i = Intersection::hit(&intersections).unwrap();
@@ -55,18 +64,9 @@ mod tests {
     #[test]
     fn hit_some_interactions_have_negative_distance() {
         let s = Sphere::new();
-        let i1 = Intersection {
-            distance: -1.0,
-            object: &s,
-        };
-        let i2 = Intersection {
-            distance: 1.0,
-            object: &s,
-        };
-        let i3 = Intersection {
-            distance: -0.5,
-            object: &s,
-        };
+        let i1 = Intersection::new(-1.0, &s);
+        let i2 = Intersection::new(1.0, &s);
+        let i3 = Intersection::new(-0.5, &s);
         let interactions = vec![i1, i2, i3];
         let i = Intersection::hit(&interactions).unwrap();
         assert_eq!(&i2, i);
@@ -75,18 +75,9 @@ mod tests {
     #[test]
     fn no_hit_when_all_interactions_negative() {
         let s = Sphere::new();
-        let i1 = Intersection {
-            distance: -2.0,
-            object: &s,
-        };
-        let i2 = Intersection {
-            distance: -1.0,
-            object: &s,
-        };
-        let i3 = Intersection {
-            distance: -0.5,
-            object: &s,
-        };
+        let i1 = Intersection::new(-2.0, &s);
+        let i2 = Intersection::new(-1.0, &s);
+        let i3 = Intersection::new(-0.5, &s);
         let interactions = vec![i1, i2, i3];
         let i = Intersection::hit(&interactions);
         assert!(i.is_none());
@@ -95,24 +86,22 @@ mod tests {
     #[test]
     fn hit_is_lowest_nonnegative_intersection() {
         let s = Sphere::new();
-        let i1 = Intersection {
-            distance: 5.0,
-            object: &s,
-        };
-        let i2 = Intersection {
-            distance: 7.0,
-            object: &s,
-        };
-        let i3 = Intersection {
-            distance: -3.0,
-            object: &s,
-        };
-        let i4 = Intersection {
-            distance: 2.0,
-            object: &s,
-        };
+        let i1 = Intersection::new(5.0, &s);
+        let i2 = Intersection::new(7.0, &s);
+        let i3 = Intersection::new(-3.0, &s);
+        let i4 = Intersection::new(2.0, &s);
         let interactions = vec![i1, i2, i3, i4];
         let i = Intersection::hit(&interactions).unwrap();
         assert_eq!(&i4, i);
+    }
+
+    #[test]
+    fn create_intersection_with_uv() {
+        let s = Sphere::new();
+        let i = Intersection::new_with_uv(1.0, &s, 0.2, 0.4);
+        assert_eq!(i.distance, 1.0);
+        assert!(ptr::eq(&s as &dyn Shape, i.object as &dyn Shape));
+        assert_eq!(i.u, 0.2);
+        assert_eq!(i.v, 0.4);
     }
 }
